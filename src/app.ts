c59a90d18b8c196cda85587088a1dbf42b44740c
Sahidul Islam Jony <jonyahmed19@gmail.com>
2023-06-30 22:29:13 +0600

    setup done

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
-import express, {Application, Request, Response} from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 import morgan from 'morgan'
-const app:Application = express();
 
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(morgan('dev'));
-
-
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+app.use(morgan('dev'))
 
 //Testing
 app.get('/healthz', async (req: Request, res: Response) => {
-    res.send('Working Successfully');
+  res.send('Working Successfully')
 })
 
-export  default app
+export default app
